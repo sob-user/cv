5b8acf76464ab1cfbc550732520ed556f8bf35d3
sob-user <48519717+sob-user@users.noreply.github.com>
2019-10-19 13:04:11 +0200

    step2maj

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    readFile('data/articles.json', function(articles) {
+    readFile('https://raw.githubusercontent.com/promo3Saint-Maur/files/master/articles.json', function(articles) {
         console.log(articles);
 
         // --------------------- STEP 0 (informations) ---------------------
@@ -23,21 +23,70 @@ window.onload = function() {
             // 1.2 DEFINIR DES GETTER/SETTER POUR CHAQUE PROPRIETES DE LA CLASS Article
                 // exemple : this.SetTitle = function(newTitle) { this.title = newTitle; } <----- ceci est un SETTER
 class Article {
-    constructor (title, author, publishedDate, img, content, resumes) {
+	id;
+    constructor (id, title, author, publishedDate, img, content, resumes, tags) {
+    	this.id = id;
         this.title = title;
         this.author = author;
         this.publishedDate = publishedDate;
         this.img = img;
         this.content = content;
         this.resumes = resumes;
+        this.tags = tags; 
     }
-    this.SetTitle = function(newTitle) {this.title = newTitle;}
-    this.SetAuthor = function(newAuthor) {this.author = author;}
-    this.SetPublishedDate = function(newPublishedDate) {this.publishedDate = publishedDate;}
-    this.SetImg = function(newImg) {this.img = img;}
-    this.SetContent = function(newContent) {this.content = content;}
-    this.SetResumes = function(newResumes) { this.resumes = resumes;}
+   
+    //this.SetId = function(newId) {this.id = newId;}
+    //this.SetTitle = function(newTitle) {this.title = newTitle; }
+    //this.SetAuthor = function(newAuthor) {this.author = newAuthor; }
+    //this.SetPublishedDate = function(newPublishedDate) {this.publishedDate = newPublishedDate; }
+    //this.SetImg = function(newImg) {this.img = newImg; }
+    //this.SetContent = function(newContent) {this.content = newContent; }
+    //this.SetTags = function(newTags) {this.tags = newTags; }
+
+
 };
+const section = document.getElementsByTagName('section')[0];
+
+
+for (var i = 0; i < articles.length; i++) {
+let article = new Article(articles[i].id, articles[i].title, articles[i].author, articles[i].publishedDate, articles[i].img, articles[i].content, articles[i].tags); 
+var articleBloc = document.createElement('article');
+articleBloc.setAttribute('data-id', article.id);
+
+articleBloc.style.display = 'flex';
+
+
+var articleTitle = document.createElement('h2');
+articleTitle.innerText = article.title;
+articleBloc.appendChild(articleTitle);
+
+var articleAuthor = document.createElement('h4');
+articleAuthor.innerText = article.author;
+articleBloc.appendChild(articleAuthor);
+
+var articlePublishedDate = document.createElement('p');
+articlePublishedDate.innerText = article.publishedDate;
+articleBloc.appendChild(articlePublishedDate);
+
+var articleImg = document.createElement('img');
+articleImg.setAttribute('src', article.img)
+articleBloc.appendChild(articleImg);
+
+var articleContent = document.createElement('p');
+articleContent.innerText = article.content;
+articleBloc.appendChild(articleContent);
+
+var articleResumes = document.createElement('p');
+articleResumes.innerText = article.resumes;
+articleBloc.appendChild(articleResumes);
+
+var articleTags = document.createElement('p');
+articleTags.innerText = article.tags;
+articleBloc.appendChild(articleTags);
+
+section.appendChild(articleBloc);
+}
+
 
 
 
@@ -90,3 +139,4 @@ class Article {
 
     });
 }
+
